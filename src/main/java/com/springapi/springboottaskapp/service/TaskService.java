70d095699eeedc74a05e9dd4d8d766b02dd9cafa
Itsoya Abebe <itsoyaa@gmail.com>
2026-03-23 15:08:55 -0400

    Update Task services with update methods

--- src/main/java/com/springapi/springboottaskapp/service/TaskService.java
+++ src/main/java/com/springapi/springboottaskapp/service/TaskService.java
@@ -1,14 +1,18 @@
 package com.springapi.springboottaskapp.service;
 
 import com.springapi.springboottaskapp.domain.CreateTaskRequest;
+import com.springapi.springboottaskapp.domain.UpdateTaskRequest;
 import com.springapi.springboottaskapp.domain.entity.Tasks;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface TaskService {
 
     Tasks createTask(CreateTaskRequest request);
 
     List<Tasks> getAllTasks();
+
+    Tasks updateTask(UUID taskId, UpdateTaskRequest request);
 
 }
--- src/main/java/com/springapi/springboottaskapp/service/impl/TaskServiceImpl.java
+++ src/main/java/com/springapi/springboottaskapp/service/impl/TaskServiceImpl.java
@@ -1,8 +1,10 @@
 package com.springapi.springboottaskapp.service.impl;
 
 import com.springapi.springboottaskapp.domain.CreateTaskRequest;
+import com.springapi.springboottaskapp.domain.UpdateTaskRequest;
 import com.springapi.springboottaskapp.domain.entity.Tasks;
 import com.springapi.springboottaskapp.domain.entity.TaskStatus;
+import com.springapi.springboottaskapp.exception.TaskNotFoundException;
 import com.springapi.springboottaskapp.repository.TaskRepository;
 import com.springapi.springboottaskapp.service.TaskService;
 import org.springframework.data.domain.Sort;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TaskServiceImpl implements TaskService {
@@ -39,5 +42,20 @@ public class TaskServiceImpl implements TaskService {
     @Override
     public List<Tasks> getAllTasks() {
         return taskRepository.findAll(Sort.by(Sort.Direction.ASC, "created"));
+    }
+
+    @Override
+    public Tasks updateTask(UUID taskId, UpdateTaskRequest request) {
+        Tasks task = taskRepository.findById(taskId)
+                .orElseThrow(() -> new TaskNotFoundException(taskId));
+
+        task.setTitle(request.title());
+        task.setDescription(request.description());
+        task.setDueDate(request.dueDate());
+        task.setPriority(request.priority());
+        task.setStatus(TaskStatus.OPEN);
+        task.setUpdated(Instant.now());
+
+        return taskRepository.save(task);
     }
 }
