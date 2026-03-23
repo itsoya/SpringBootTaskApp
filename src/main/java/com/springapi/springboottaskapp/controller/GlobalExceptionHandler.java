2a71afde74c8a091ae5e7f5ac1655db6b9ea528b
Itsoya Abebe <itsoyaa@gmail.com>
2026-03-23 15:26:41 -0400

    update exceptions for id not found

--- src/main/java/com/springapi/springboottaskapp/controller/GlobalExceptionHandler.java
+++ src/main/java/com/springapi/springboottaskapp/controller/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.springapi.springboottaskapp.controller;
 
 import com.springapi.springboottaskapp.domain.dto.ErrorDto;
 import com.springapi.springboottaskapp.domain.entity.Tasks;
+import com.springapi.springboottaskapp.exception.TaskNotFoundException;
 import jakarta.validation.ValidationException;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
@@ -9,6 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.UUID;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -16,7 +20,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<ErrorDto> handleValidationException(MethodArgumentNotValidException ex) {
         String errorMessage = ex.getBindingResult().getFieldErrors().stream().findFirst().map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .orElse(ex.getMessage());
+                .orElse("Validation failed");
+        ErrorDto errorDto = new ErrorDto(errorMessage);
+        return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler
+    public ResponseEntity<ErrorDto> handleTaskNotFoundException(TaskNotFoundException ex) {
+        UUID taskNotFoundId = ex.getId();
+        String errorMessage = String.format("Task with id %s not found", taskNotFoundId);
         ErrorDto errorDto = new ErrorDto(errorMessage);
         return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
     }
